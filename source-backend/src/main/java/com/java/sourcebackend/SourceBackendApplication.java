1541d29f1bad5898433650602d238c1c32b6f2d1
khoahoang555 <khoahoangyahoo@gmail.com>
2023-03-25 09:32:05 +0700

    first-commit: Create source backend

---
+++ source-backend/src/main/java/com/java/sourcebackend/SourceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.sourcebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceBackendApplication.class, args);
+    }
+
+}
---
+++ source-backend/src/test/java/com/java/sourcebackend/SourceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.sourcebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
