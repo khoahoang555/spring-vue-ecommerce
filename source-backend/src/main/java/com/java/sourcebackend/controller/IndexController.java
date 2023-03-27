bdf01b752764456f9e4faaf320536b2d7b1d6593
khoahoang555 <khoahoangyahoo@gmail.com>
2023-03-27 08:27:04 +0700

    config-source-code: Config docker(upload file Dockerfile)

---
+++ source-backend/src/main/java/com/java/sourcebackend/controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.java.sourcebackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    public String index() {
+        return "Health check!";
+    }
+}
